ff91c46daed8497f8fbf143b9ec9ed189afc9be3
Colin Beeby <colin.beeby1@nhs.net>
2025-06-26 17:12:49 +0100

    Added first part of CICD pipeline for dev environment

---
+++ main.tf
@@ -0,0 +1,48 @@
+resource "azurerm_resource_group" "MessageQueueProcessorResourceGroup" {
+  name        = var.ResourceGroupName
+  location    = var.ResourceGroupLocation
+}
+
+resource "azurerm_storage_account" "MessageQueueProcessorStorageAccount" {
+  name                       = "messagequeueprocessorsa"
+  resource_group_name        = azurerm_resource_group.MessageQueueProcessorResourceGroup.name
+  location                   = azurerm_resource_group.MessageQueueProcessorResourceGroup.location
+  account_tier               = "Standard"
+  account_replication_type   = "LRS"
+}
+
+resource "azurerm_app_service_plan" "MessageQueueProcessorAppServicePlan" {
+  name                       = "MessageQueueProcessorAppServicePlan"
+  resource_group_name        = azurerm_resource_group.MessageQueueProcessorResourceGroup.name
+  location                   = azurerm_resource_group.MessageQueueProcessorResourceGroup.location
+  kind                       = "FunctionApp"
+  reserved                   = true
+  sku {
+    tier = "Dynamic"
+    size = "Y1"
+  }
+
+resource "azurerm_function_app" "MessageQueueProcessorFunctionApp" {
+  name                       = "MessageQueueProcessorApp"
+  resource_group_name        = azurerm_resource_group.MessageQueueProcessorResourceGroup.name
+  location                   = azurerm_resource_group.MessageQueueProcessorResourceGroup.location
+  app_service_plan_id        = azurerm_app_service_plan.MessageQueueProcessorAppServicePlan.id
+  storage_account_name       = azurerm_storage_account.MessageQueueProcessorStorageAccount.name
+  storage_account_access_key = azurerm_storage_account.MessageQueueProcessorStorageAccount.primary_access_key
+  version                    = "~4"
+  site_config {
+    application_stack {
+      dotnet_version = "8.0"
+    }
+    use_32_bit_worker_process = false
+    always_on                 = true
+    ftps_state                = "Disabled"
+    scm_type                  = "None"
+  }
+  app_settings = {
+    FUNCTIONS_WORKER_RUNTIME = "dotnet-isolated"
+    WEBSITE_RUN_FROM_PACKAGE = "~4"
+    AzureWebJobsStorage      = azurerm_storage_account.MessageQueueProcessorStorageAccount.primary_connection_string"
+    WEBSITE_RUN_FROM_PACKAGE = "1"
+  }
+}
