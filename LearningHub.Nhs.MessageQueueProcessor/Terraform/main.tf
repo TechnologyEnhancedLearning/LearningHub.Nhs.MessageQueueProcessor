3bf5ba1b0b9a94d1b5ec6ef9308e71c4c40ee59d
Colin Beeby <colin.beeby1@nhs.net>
2025-07-01 13:42:41 +0100

    Corrected security rule definitions

--- LearningHub.Nhs.MessageQueueProcessor/Terraform/main.tf
+++ LearningHub.Nhs.MessageQueueProcessor/Terraform/main.tf
@@ -124,6 +124,7 @@ resource "azurerm_network_security_group" "nsg" {
     priority                   = 104
     protocol                   = "*"
     source_port_range          = "*"
+    destination_port_range     = "*"
   }
   security_rule {
     name                       = "Microsoft.Sql-managedInstances_UseOnly_mi-strg-s-out-10-0-1-0-24-v11"
