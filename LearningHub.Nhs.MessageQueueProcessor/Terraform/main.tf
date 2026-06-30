7e79b03e989d04a4d4b761e4f44dcea4f8fd5c4e
Colin Beeby <colin.beeby1@nhs.net>
2026-06-30 16:36:11 +0100

    Temporarily added the subnet import to terraform

--- LearningHub.Nhs.MessageQueueProcessor/Terraform/main.tf
+++ LearningHub.Nhs.MessageQueueProcessor/Terraform/main.tf
@@ -180,7 +180,7 @@ resource "azurerm_route_table" "route_table" {
   resource_group_name = azurerm_resource_group.MessageQueueProcessorResourceGroup.name
 }
 
-resource "azurerm_subnet" "subnet" {
+resource "azurerm_subnet" "subnet2" {
   name = "ManagedInstanceSubnet2"
   resource_group_name = azurerm_resource_group.MessageQueueProcessorResourceGroup.name
   virtual_network_name = azurerm_virtual_network.vnet.name
